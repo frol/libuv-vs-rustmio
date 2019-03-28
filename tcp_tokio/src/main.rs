8e900840c819a1e64f7ad28f7277aca03aedaa41
Alexander Polakov <plhk@sdf.org>
2019-03-29 02:33:30 +0300

    Alternative tokio impl

--- tcp_tokio/src/main.rs
+++ tcp_tokio/src/main.rs
@@ -1,6 +1,47 @@
-use tokio::io;
+use tokio::codec::Framed;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
+use tokio::codec::{Encoder, Decoder};
+use tokio::runtime::current_thread::Runtime;
+
+use bytes::{BufMut, BytesMut};
+
+struct C;
+
+impl Encoder for C {
+    type Item = u64;
+    type Error = std::io::Error;
+    fn encode(
+        &mut self,
+        item: Self::Item,
+        dst: &mut BytesMut
+    ) -> Result<(), Self::Error> {
+        const PONG: &[u8] = b"+PONG\r\n";
+        for _ in 0..item {
+            dst.reserve(PONG.len());
+            dst.put(PONG.as_ref());
+        }
+        Ok(())
+    }
+}
+
+impl Decoder for C {
+    type Item = u64;
+    type Error = std::io::Error;
+
+    fn decode(
+        &mut self,
+        src: &mut BytesMut
+    ) -> Result<Option<Self::Item>, Self::Error> {
+        let count = bytecount::count(&src, b'\n') as u64;
+        if count > 0 {
+            src.clear();
+            Ok(Some(count))
+        } else {
+            Ok(None)
+        }
+    }
+}
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let addr = "127.0.0.1:8888".parse()?;
@@ -12,19 +53,14 @@ fn main() -> Result<(), Box<std::error::Error>> {
         .incoming()
         .map_err(|e| println!("failed to accept socket; error = {:?}", e))
         .for_each(move |socket| {
-            let (reader, writer) = socket.split();
-            tokio::spawn(
-                io::lines(std::io::BufReader::new(reader))
-                    .filter(|line| line.starts_with("PING"))
-                    .fold(
-                        writer,
-                        |writer, _| {
-                            io::write_all(writer, b"+PONG\r\n").map(|(w, _)| w)
-                        })
-                    .then(|_| Ok(()))
-            )
+            tokio::spawn({
+                let framed = Framed::new(socket, C);
+                let (writer, reader) = framed.split();
+                reader.forward(writer).map_err(|_| ()).map(|_| ())
+            })
         });
 
-    tokio::run(done);
+    let mut rt = Runtime::new().unwrap();
+    rt.block_on(done).unwrap();
     Ok(())
 }
