996dd55719d4eec6dbe42aa69713bc65ddaf4425
Vlad Frolov <frolvlad@gmail.com>
2019-03-28 23:41:42 +0200

    Added python implementation using asyncio and libuv

---
+++ tcp_libuv_python/README.md
@@ -0,0 +1,20 @@
+Python Implementation of Redis PING protocol
+============================================
+
+This implementation uses asyncio with libuv event loop.
+
+## Preparation
+
+Install requirements:
+
+```
+$ pip install -r requirements.txt
+```
+
+NOTE: It is recommended to use virtualenv or other virtual environments.
+
+## Execute
+
+```
+$ python main.py
+```
---
+++ tcp_libuv_python/main.py
@@ -0,0 +1,22 @@
+import asyncio
+
+class MyProtocol(asyncio.Protocol):
+    def __init__(self):
+        self.transport = None
+
+    def connection_made(self, transport: asyncio.transports.Transport):
+        self.transport = transport
+
+    def data_received(self, data: bytes):
+        number_of_commands = data.count(b"\r\n")
+        self.transport.writelines([b"+PONG\r\n"] * number_of_commands)
+
+def main():
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(MyProtocol, "127.0.0.1", 8888)
+    server = loop.run_until_complete(coro)
+    loop.run_forever()
+
+main()
