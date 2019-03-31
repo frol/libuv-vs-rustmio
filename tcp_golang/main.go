342a5689ea95a2a00a84e13f9fe2e0d45aa3ca87
Vlad Frolov <frolvlad@gmail.com>
2019-03-31 18:33:38 +0300

    Fixed the github crazyness

--- tcp_golang/main.go
+++ tcp_golang/main.go
@@ -10,38 +10,6 @@ func handleRequest(conn net.Conn) {
 	buf := make([]byte, 4096)
 	for {
 		var count int
-		_, err := conn.Read(buf)
-		if err != nil {
-			break
-		}
-		for i := 0; i < len(buf); i++ {
-			if buf[i] == '\n' {
-				count++
-			}
-		}
-		conn.Write([]byte(strings.Repeat("+PONG\r\n", count)))
-	}
-
-}
-func main() {
-	listener, err := net.Listen("tcp", "127.0.0.1:8888")
-	if err != nil {
-		panic(err)
-	}
-
-	defer listener.Close()
-package main
-
-import (
-	"net"
-	"strings"
-)
-
-func handleRequest(conn net.Conn) {
-	defer conn.Close()
-	buf := make([]byte, 4096)
-	for {
-		var count int
 		n, err := conn.Read(buf)
 		if err != nil {
 			break
@@ -53,8 +21,8 @@ func handleRequest(conn net.Conn) {
 		}
 		conn.Write([]byte(strings.Repeat("+PONG\r\n", count)))
 	}
-
 }
+
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:8888")
 	if err != nil {
@@ -63,13 +31,6 @@ func main() {
 
 	defer listener.Close()
 
-	for {
-		// skip errors
-		conn, _ := listener.Accept()
-
-		go handleRequest(conn)
-	}
-}
 	for {
 		// skip errors
 		conn, _ := listener.Accept()
